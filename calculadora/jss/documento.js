0c77516e366149800a3a87925d717272c116499c
Daniel <dani2014games@gmail.com>
2023-03-29 01:55:45 -0300

    First commit

---
+++ calculadora/jss/documento.js
@@ -0,0 +1,8 @@
+document.write('<label for="n1">Número 1</label><br>');
+document.write('<input type="number" name="nmbNumero1" id="n1" class="n1"><br><br>');
+document.write('<label for="n2">Número 2</label><br>');
+document.write('<input type="number" name="nmbNumero2" id="n2" class="n2"><br><br>');
+document.write('<input type="submit" value="Calcular" class = "calc">'); 
+
+//Isso é apenas um teste de JS, na vida real de um DEV nunca devemos fazer isso pois gera dificuldade de manutenção
+                                                                           
