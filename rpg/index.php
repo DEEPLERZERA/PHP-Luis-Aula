d1f8802a061fc148135449b7f66adf284df9e2f2
Daniel <dani2014games@gmail.com>
2023-06-02 02:02:14 -0300

    Adding new things to rpg game

--- rpg/index.php
+++ rpg/index.php
@@ -4,51 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href = "./css/style.css" />
-    <title>Ficha de RPG</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>FICHA</title>
 </head>
 <body>
-    
-    <form action="Personagem.php" method="post"> Personagem
-        <div class="container">
-            <div class="box">
-                <div class="box-cell box1">
-                    <label for="nivel">Nivel<br></label>
-                        <input type="number" name="nbmNivel" id="nivel"><br>
-                    <label for="nome">Nome<br></label>
-                        <input type="text" name="nome" id="nome"><br>
-                        <label for="raca">Raça<br></label>
-                        <input type="text" name="raca" id="raca"><br>
-                        <label for="profissao">Profissão<br></label>
-                        <input type="text" name="profissao" id="profissao"><br>
-                        <label for="constituicao">Constituição<br></label>
-                        <input type="number" name="constituicao" id="constituicao"><br>
-                        <label for="destreza">Destreza<br></label>
-                        <input type="number" name="destreza" id="destreza"><br>
-                        <label for="forca">Força<br></label>
-                        <input type="number" name="forca" id="forca"><br>
-                        <label for="inteligencia">Inteligência<br></label>
-                        <input type="number" name="inteligencia" id="inteligencia"><br>
-                        <label for="sabedoria">Sabedoria<br></label>
-                        <input type="number" name="sabedoria" id="sabedoria"><br>
-                    </div>
-                    <div class="box-cell box2">   
-                        <label for="carisma">Carisma<br></label>
-                        <input type="number" name="carisma" id="carisma"><br>
-                        <label for="arma">Arma<br></label>
-                        <input type="text" name="arma" id="arma"><br>
-                        <label for="categoria">Categoria de Armadura<br></label>
-                        <input type="number" name="categoria" id="categoria"><br>
-                        <label for="vida">Pontos de Vida<br></label>
-                        <input type="number" name="vida" id="vida"><br>
-                        <label for="magiaDia">Qt de Magias por Dia<br></label>
-                        <input type="number" name="magiaDia" id="magiaDia"><br>
-                        <label for="cadastrar">Cadastrar<br></label>
-                        <input type="submit" name="cadastrar" id="cadastrar"><br>
-                    </div>
-                </div>
-            </div>
+    <div class="container">
+        <h1>Ficha RPG</h1>
+        <div class="centralDiv">
+            <div class="formHell">                
+                 
+                            
+                <?php
+                // Verifica se o formulário foi submetido
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    // Verifica se o valor A foi confirmado
+                    if (isset($_POST["confirmar"]) && $_POST["confirmar"] == "A") {
+                        echo ("header['location: produto-lista.php'];");
+                    }
+                }
+                ?>
+
+                <!-- Formulário com o botão de confirmação -->
+                <form class = "hellPost" method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+                    <input type="hidden" name="confirmar" value="A">
+                    <input type="submit" value="Confirmar">
+                </form>
+                        
+                   
+            </div>            
         </div>
-    </form>
+    </div>
 </body>
 </html>
