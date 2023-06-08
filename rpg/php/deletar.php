a5545ddea83d51d0047a76b3d4fa9cf1fa930d0a
Daniel <dani2014games@gmail.com>
2023-06-08 17:53:13 -0300

    Atualizações

---
+++ rpg/php/deletar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DELETAR FICHA</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="shortcut icon" href="../image/duvida.png" type="image/x-icon">
+</head>
+<body>
+    <div class="container">
+        <h1>Ficha RPG</h1>
+        <div class="centralDiv">
+            <div class="formHell">    
+                <div class = "formHell2">            
+                 <form class ="formHellPost" action="../index.php" method="delete">    
+                    <label for = "btnForm2" class = "text1"> Digite o ID para deletar </label>            
+                    <input type = "text" class = "btnForm2">
+                </div>
+                <input class = "btnForm3" type="submit" value="Deletar">
+                </form>
+            </div>            
+        </div>
+    </div>
+</body>
+</html>
